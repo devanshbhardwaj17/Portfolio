18522fc0fe33e8dc6fd616b45d410927d732b77a
DEVANSH <devanshb661@gmail.com>
2025-10-30 13:16:13 +0530

    Experiment 2

--- src/componets/ReviewCard2.jsx
+++ src/componets/ReviewCard2.jsx
@@ -4,7 +4,7 @@ export default function ReviewCard2(props) {
   return (
     <div className=' p-2 bg-gray-800 bg-opacity-50 rounded-xl shadow-xl font-sans w-full flex-shrink-0'>
         <div className=' w-full flex justify-center overflow-hidden mt-4'>
-            <img src={props.pic} alt="" className=' sm:w-[340px] w-[90%] h-[300px] sm:h-[350px] rounded-xl shadow-2xl' />
+            <img src={props.pic} alt="" className=' sm:w-[340px] w-[80%] h-[300px] sm:h-[350px] rounded-xl shadow-2xl' />
         </div>
         <div className=' mt-2'>
             <p className=' text-center text-4xl text-yellow-200 font-semibold'>{props.name}</p>
