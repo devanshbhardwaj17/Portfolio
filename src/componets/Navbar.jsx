ebcd3d0d57efdc02032319660d395d502716348c
DEVANSH <devanshb661@gmail.com>
2025-10-31 10:02:49 +0530

    Change

--- src/componets/Navbar.jsx
+++ src/componets/Navbar.jsx
@@ -8,7 +8,7 @@ export default function Navbar() {
             <div className=' px-4 mt-3'>
                 <h1 className=' sm:text-3xl text-2xl font-sans font-semibold text-white  bg-gray-800 bg-opacity-90 px-5 rounded-lg'>Portfolio</h1>
             </div>
-            <div className=' bg-gray-800 bg-opacity-90 md:w-[50%] sm:w-[60%] w-full float-end sm:rounded-bl-xl sm:py-3 shadow-sm shadow-[#565656] mt-2 sm:mt-0 mx-3 py-1 rounded-2xl'>
+            <div className=' bg-gray-800 bg-opacity-90 md:w-[50%] sm:w-[60%] w-full float-end sm:rounded-bl-xl sm:py-3 shadow-sm shadow-[#565656] mt-2 sm:mt-0 mx-3 py-1 rounded-2xl sm:rounded-none sm:mx-0'>
                 <ul className=' flex justify-evenly items-center font-sans'>
                     <NavLink
                     to="/"
