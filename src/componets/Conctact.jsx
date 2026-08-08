93c9c6222f3c1c6ded51fc46de8e46179cb63426
DEVANSH <devanshb661@gmail.com>
2026-08-08 22:44:25 +0530

    Changes in Contact Form

--- src/componets/Conctact.jsx
+++ src/componets/Conctact.jsx
@@ -1,6 +1,26 @@
 import React from 'react'
 import { FaFileAlt, FaMapMarkerAlt, FaPaperPlane, } from 'react-icons/fa'
+import { useState } from 'react';
+
+
 export default function Conctact() {
+
+    const [result, setResult] = useState("");
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+        formData.append("access_key", "bbeca402-0af8-4e51-8a40-f671ccb5f970");
+
+        const response = await fetch("https://api.web3forms.com/submit", {
+            method: "POST",
+            body: formData
+        });
+
+        const data = await response.json();
+        setResult(data.success ? "Success!" : "Error");
+        alert(result)
+    };
     //Done
     return (
         <>
@@ -19,15 +39,16 @@ export default function Conctact() {
                 <FaFileAlt className=' text-5xl text-white bg-gray-500 bg-opacity-40 p-2 rounded-xl' />
                 <p className=' text-white text-3xl font-bold'>Contact Form</p>
             </div>
-            <form action="" className=' md:mx-5 mx-1 mb-7 py-3 px-2 flex flex-wrap justify-evenly bg-gray-700 rounded-2xl'>
-                <input type="text" name="" id="" placeholder='Name' className=' text-lg md:w-[45%] w-full bg-gray-600 bg-opacity-90 p-2 rounded-xl shadow-xl text-white placeholder:text-gray-300 md:my-0 my-2' />
-                <input type="text" name="" id="" placeholder='Address' className=' text-lg md:w-[45%] w-full bg-gray-600 bg-opacity-90 p-2 rounded-xl shadow-xl text-white placeholder:text-gray-300 md:my-0 my-2' />
-                <input type="text" name="" id="" placeholder='Email' className=' text-lg md:w-[45%] w-full bg-gray-600 bg-opacity-90 p-2 rounded-xl shadow-xl text-white placeholder:text-gray-300 md:my-3 my-2' />
-                <input type='tels' name="" id="" placeholder='Mobile' className=' text-lg  md:w-[45%] w-full bg-gray-600 bg-opacity-90 p-2 rounded-xl shadow-xl text-white placeholder:text-gray-300 md:my-3 my-2' />
-                <textarea name="" id="" rows={8} placeholder='Message' className=' text-lg md:w-[94%] w-full bg-gray-600 bg-opacity-90 p-2 rounded-xl shadow-xl text-white placeholder:text-gray-300 md:my-0 my-2' />
+            <form onSubmit={onSubmit} className=' md:mx-5 mx-1 mb-7 py-3 px-2 flex flex-wrap justify-evenly bg-gray-700 rounded-2xl'>
+                <input type="text" name="name" required placeholder='Name' className=' text-lg md:w-[45%] w-full bg-gray-600 bg-opacity-90 p-2 rounded-xl shadow-xl text-white placeholder:text-gray-300 md:my-0 my-2' />
+                <input type="text" name="address" required placeholder='Address' className=' text-lg md:w-[45%] w-full bg-gray-600 bg-opacity-90 p-2 rounded-xl shadow-xl text-white placeholder:text-gray-300 md:my-0 my-2' />
+                <input type="email" name="email" required placeholder='Email' className=' text-lg md:w-[45%] w-full bg-gray-600 bg-opacity-90 p-2 rounded-xl shadow-xl text-white placeholder:text-gray-300 md:my-3 my-2' />
+                <input type="tel" name="mobile" required placeholder='Mobile No.' className=' text-lg md:w-[45%] w-full bg-gray-600 bg-opacity-90 p-2 rounded-xl shadow-xl text-white placeholder:text-gray-300 md:my-3 my-2' />
+
+                <textarea name="message" required rows={8} placeholder='Message' className=' text-lg md:w-[94%] w-full bg-gray-600 bg-opacity-90 p-2 rounded-xl shadow-xl text-white placeholder:text-gray-300 md:my-0 my-2'></textarea>
                 <div className=' w-full mt-2 md:px-5 flex justify-center md:justify-end'>
-                    <button className='  bg-gray-600 bg-opacity-90 md:text-xl font-semibold font-sans shadow-2xl md:px-3 text-center w-full md:w-auto text-3xl rounded-lg text-yellow-500 flex justify-center md:justify-start gap-3'>
-                        <FaPaperPlane className=' p-1 text-3xl'/><p><span></span>Send Message</p>
+                    <button type="submit" className='  bg-gray-600 bg-opacity-90 md:text-xl font-semibold font-sans shadow-2xl md:px-3 text-center w-full md:w-auto text-3xl rounded-lg text-yellow-500 flex justify-center md:justify-start gap-3 hover:text-white hover:scale-105 duration-200'>
+                        <FaPaperPlane className=' p-1 text-3xl' /><p><span></span>Send Message</p>
                     </button>
                 </div>
             </form>
