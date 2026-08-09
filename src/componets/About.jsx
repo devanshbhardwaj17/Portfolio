da41fe37e52431ee8ab2fad33859706edfa38197
DEVANSH <devanshb661@gmail.com>
2026-08-09 16:32:36 +0530

    Update About.jsx

--- src/componets/About.jsx
+++ src/componets/About.jsx
@@ -11,17 +11,17 @@ export default function About() {
         <div className=' py-2 md:px-5 px-2 font-sans'>
             <h2 className=' text-3xl font-bold text-white'>About Me</h2>
             <div className='w-[10%] p-0.5 bg-[rgb(255,215,0)] my-3 rounded-lg'></div>
-            <p className=' text-white font-thin text-lg my-7'>I am a <span className=' text-white font-semibold'>Frontend Web Developer</span> from <span className=' text-white font-semibold'>Kasganj Uttar Pradesh, India</span>. I build the projects using <span className=' text-white font-semibold'>HTML, CSS, JavaScript, React, TailwindCSS</span>.I enjoy turning complex problems into simple, beautiful and intuitive designs. <br /> <br />My job is to build your website so that it is functional and user-friendly but at the same time attractive. Moreover, I add personal touch to your product and make sure that is eye-catching and easy to use. My aim is to bring across your message and identity in the most creative way.</p>
+            <p className=' text-white font-thin text-lg my-7'>I am a <span className=' text-white font-semibold'>Frontend Web Developer</span> from <span className=' text-white font-semibold'>Kasganj Uttar Pradesh, India</span>. I build the projects using <span className=' text-white font-semibold'>HTML, CSS, JavaScript, React, TailwindCSS</span>. I enjoy turning complex problems into simple, beautiful and intuitive designs. <br /> <br />My job is to build your website so that it is functional and user-friendly but at the same time attractive. Moreover, I add personal touch to your product and make sure that is eye-catching and easy to use. My aim is to bring across your message and identity in the most creative way.</p>
             <h2 className=' text-3xl font-bold text-white mb-2'>What I'm Doing</h2>
             <div className=' flex flex-wrap gap-3'>
                 <JobCard
                     image={Photo1}
-                    title="Web Desing"
+                    title="Web Design"
                     value="The most modern and high-quality design made at a professional level."
                 />
                 <JobCard
                     image={Photo2}
-                    title="Web Develpoment"
+                    title="Web Development"
                     value="High-quality development of sites at the professional level."
                 />
                 <JobCard
